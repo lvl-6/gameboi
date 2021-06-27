431a51ce5ee786187fd206f5a26e47a5466f4760
John C <exampleman@example.com>
2021-06-27 15:12:00 +0100

    Added an actual README of sorts, and some comments to database.py.

--- lib/database.py
+++ lib/database.py
@@ -10,20 +10,20 @@
 ###############################################################################
 
 import mariadb # May require rewrite if I end up using another DBMS
-import sys
 import os
 
 ###############################################################################
 # Configuration Variables
 ###############################################################################
+# TODO: Don't use environment variables here - get them from a file instead.
 
 # Edit these to point it to a different server/database.
 # Using environment variables to ensure no passwords accidentally committed.
-user = os.getenv('GB_DB_USER')
-password = os.getenv('GB_DB_PASSWORD')
-host = os.getenv('GB_DB_HOST')
-port = int(os.getenv('GB_DB_PORT'))
-database = 'gameboi'
+user = os.getenv('GB_DB_USER')          # User (set up on database)
+password = os.getenv('GB_DB_PASSWORD')  # User's password
+host = os.getenv('GB_DB_HOST')          # IP address of the database server
+port = int(os.getenv('GB_DB_PORT'))     # Port the server is on (default 3306)
+database = 'gameboi'                    # The specific database on that server
 
 ###############################################################################
 # Class: Database
@@ -47,7 +47,6 @@ class Database:
             print(f'Error connecting to MariaDB platform: {e}')
         self.cur = self.conn.cursor()
 
-    @staticmethod
     def test_db(self):
         """Just a test function to make sure all is working, TODO: Delete"""
         self.cur.execute("SELECT id,name FROM test")
