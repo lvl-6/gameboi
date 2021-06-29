0197a3349e14433559bca701f4481106da41678d
John C <24364368+lvl-6@users.noreply.github.com>
2021-06-29 22:09:27 +0100

    Fleshed out the Game class.

--- lib/game.py
+++ lib/game.py
@@ -10,13 +10,70 @@
 # 
 ###############################################################################
 
+import lib.database
+import mariadb
+
+db = lib.database.db
+
 
 ###############################################################################
 # Class: Game
 ###############################################################################
 
 class Game:
-    pass
+    """
+    Holds information about an individual game.
+
+    Variables:
+        pkid (int): The primary key from the database which refers to this game.
+        name (str): The name of the game.
+        icon (str): The path to the game's icon.
+        links (dict): Relevant links i.e. Steam page. Key: name of link, value: link.
+    """
+
+    pkid = None
+    name = ''
+    icon = ''
+    links = {}
+
+    def __init__(self):
+        """
+        Constructor.
+        """
+        pass
+
+    def set_pkid(self, pkid):
+        """
+        Simple little setter for pkid.
+        @param pkid: the id we want to set self.pkid to.
+        @return: None.
+        """
+        self.pkid = pkid
+
+    def get_game_data(self):
+        """
+        Pull the basic game data from the database once the pkid is known and set.
+        """
+        if self.pkid is None:
+            raise Exception("self.pkid is None - set it before calling this function!")
+
+        db.open()
+        try:
+            db.cur.execute("SELECT name,icon FROM games WHERE id=%s", (self.pkid,))
+            for(name, icon) in db.cur:
+                # Debug print
+                print(
+                    "Pulled game data...\n"
+                    "ID:\t\t" + str(self.pkid) + "\n"
+                    + "Name:\t\t" + name + "\n"
+                    + "Icon Path:\t" + icon
+                )
+                # Set the values
+                self.name = name
+                self.icon = icon
+        except mariadb.Error as e:
+            print(f"SQL error receiving game details from database: {e}")
+        db.close()
 
 
 ###############################################################################
